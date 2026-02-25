0f683f3c14ebabc39a52219fabb71da6899ebcb9
zhina <vizhvizhvizh1@gmail.com>
2026-02-25 21:41:02 +0330

    hhhhhh

--- birthdayGirl.go
+++ birthdayGirl.go
@@ -1,7 +1,12 @@
 package kim
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/zhina13/kourtney"
+)
 
 func Brt(a int) {
 	fmt.Println("you just turned", a)
+	kourtney.Doyouremember(a)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zhina13/kim
 
 go 1.25.7
+
+require github.com/zhina13/kourtney v0.0.0-20260225180256-d97eefe6a490
